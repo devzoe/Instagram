596c47727ae4d20b55d3281213150800be9ecd81
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-01-01 05:08:09 +0900

    add : sign up sign in view

---
+++ instagram_ios_zoe/Source/SignUp_SignIn/SingUpSignInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SingUpSignInViewController.swift
+//  instagram_ios_zoe
+//
+//  Created by 남경민 on 2023/01/01.
+//
+
+import UIKit
+
+class SingUpSignInViewController: UIViewController {
+
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.signUpButton.setCornerRadius(10)
+    }
+    
+    @IBAction func signUpButtonClicked(_ sender: UIButton) {
+        
+    }
+    @IBAction func signInButtonClicked(_ sender: UIButton) {
+        
+    }
+    
+
+}
