13ab72edf610fa360a75179d7f6fec58790a36df
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-01-01 05:09:30 +0900

    add : UIButton extension

---
+++ instagram_ios_zoe/Util/Extension/UIButton.swift
@@ -0,0 +1,14 @@
+//
+//  UIButton.swift
+//  instagram_ios_zoe
+//
+//  Created by 남경민 on 2023/01/01.
+//
+
+import UIKit
+
+extension UIButton {
+    func setCornerRadius(_ cornerRadius: CGFloat) {
+        self.layer.cornerRadius = cornerRadius
+    }
+}
