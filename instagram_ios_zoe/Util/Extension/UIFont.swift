a6a9f8736ac4db75b6bfb54efb985c44a2dc18e3
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-01-02 08:07:28 +0900

    add : extension String, UIColor, UIFont / IndicatorView update : UIViewController

---
+++ instagram_ios_zoe/Util/Extension/UIFont.swift
@@ -0,0 +1,16 @@
+//
+//  UIFont.swift
+//  instagram_ios_zoe
+//
+//  Created by 남경민 on 2023/01/02.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+
+    static func billabong(size: CGFloat) -> UIFont {
+        return UIFont(name: "billabong", size: size)!
+    }
+}
--- instagram_ios_zoe/Util/Extension/UIViewController.swift
+++ instagram_ios_zoe/Util/Extension/UIViewController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 extension UIViewController {
     
+    // MARK: 빈 화면을 눌렀을 때 키보드가 내려가도록 처리
+    func dismissKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer =
+            UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+//        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        self.view.endEditing(false)
+    }
+    
     // MARK: UIWindow의 rootViewController를 변경하여 화면전환
     func changeRootViewController(_ viewControllerToPresent: UIViewController) {
         if let window = UIApplication.shared.windows.first {
@@ -18,5 +30,47 @@ extension UIViewController {
             viewControllerToPresent.modalPresentationStyle = .overFullScreen
             self.present(viewControllerToPresent, animated: true, completion: nil)
         }
+    }
+    
+    // MARK: 취소와 확인이 뜨는 UIAlertController
+    func presentAlert(title: String, message: String? = nil,
+                      isCancelActionIncluded: Bool = false,
+                      preferredStyle style: UIAlertController.Style = .alert,
+                      handler: ((UIAlertAction) -> Void)? = nil) {
+        self.dismissIndicator()
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        let actionDone = UIAlertAction(title: "확인", style: .default, handler: handler)
+        alert.addAction(actionDone)
+        if isCancelActionIncluded {
+            let actionCancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+            alert.addAction(actionCancel)
+        }
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: 커스텀 UIAction이 뜨는 UIAlertController
+    func presentAlert(title: String, message: String? = nil,
+                      isCancelActionIncluded: Bool = false,
+                      preferredStyle style: UIAlertController.Style = .alert,
+                      with actions: UIAlertAction ...) {
+        self.dismissIndicator()
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        actions.forEach { alert.addAction($0) }
+        if isCancelActionIncluded {
+            let actionCancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+            alert.addAction(actionCancel)
+        }
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: 인디케이터 표시
+    func showIndicator() {
+        IndicatorView.shared.show()
+        IndicatorView.shared.showIndicator()
+    }
+    
+    // MARK: 인디케이터 숨김
+    @objc func dismissIndicator() {
+        IndicatorView.shared.dismiss()
     }
 }
